f8430cbba62372583a1bfad082652264b7d7ee08
josearcos2023 <jose.arcos@tecsup.edu.pe>
2024-05-09 16:57:48 -0500

    third exercise

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,9 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  // Controller para el campo de entrada
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,9 +30,18 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            TextFormField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Ingresa un valor',
+              ),
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                Navigator.pushNamed(context, '/page2');
+                String valorIngresado = _controller.text;
+                Navigator.pushNamed(context, '/page2',
+                    arguments: valorIngresado);
               },
               child: Text('Ir usando rutas'),
             )
@@ -43,17 +55,29 @@ class HomePage extends StatelessWidget {
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String valorRecibido =
+        ModalRoute.of(context)!.settings.arguments as String;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Segunda página'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Volver a HomePage
-            Navigator.pop(context);
-          },
-          child: Text('Regresar a Tipos de navegación'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Mostrar el valor recibido
+            Text('$valorRecibido'),
+            SizedBox(height: 20),
+            // Botón para regresar a la primera página
+            ElevatedButton(
+              onPressed: () {
+                // Volver a la primera página
+                Navigator.pop(context);
+              },
+              child: Text('Regresar a Tipos de navegación'),
+            ),
+          ],
         ),
       ),
     );
