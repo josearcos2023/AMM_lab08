526398df6fdc5ba5ea6244dcaac64ec5343b7afe
josearcos2023 <jose.arcos@tecsup.edu.pe>
2024-05-09 16:33:11 -0500

    First exercise

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tipos de navegación'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: Text('Ir usando navegación simple'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Segunda página'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Volver a HomePage
+            Navigator.pop(context);
+          },
+          child: Text('Regresar a Tipos de navegación'),
+        ),
+      ),
+    );
+  }
+}
